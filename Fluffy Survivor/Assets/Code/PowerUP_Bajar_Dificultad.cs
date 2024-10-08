8ea87f13914c30d2dc5841b4603337233576a6f5
LaguardiaOraa <anderlaguardiaoraa@gmail.com>
2024-10-08 18:29:02 +0200

    Implementado PowerUP
    Terminado la aparición del power UP, falta la lógica de aparición segun dificultad.

---
+++ Fluffy Survivor/Assets/Code/PowerUP_Bajar_Dificultad.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUP_Bajar_Dificultad : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player") // Verifica si colisiona con Jugador
+        {
+            //Aqui el codigo
+            this.gameObject.SetActive(false);
+        }
+    }
+}
